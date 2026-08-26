a9eac07ed175b38ea29585d4036be9f81141814e
Michael E. Ershov <i@emershov.ru>
2026-08-26 12:23:30 +0800

    added handling PORT

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const rawPort = process.env.PORT ?? '3000';
+  const port = Number(rawPort);
+
+  if (!Number.isInteger(port) || port < 1 || port > 65535) {
+    throw new Error(`Invalid PORT value. Expected an integer.`);
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
