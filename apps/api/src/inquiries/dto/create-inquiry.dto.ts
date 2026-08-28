7d8b851a1e59dbfea6930f9c597cb0be23694c5e
Michael E. Ershov <i@emershov.ru>
2026-08-29 00:49:40 +0800

    added entity and create-dto

---
+++ apps/api/src/inquiries/dto/create-inquiry.dto.ts
@@ -0,0 +1,27 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsOptional, IsString, Length, MaxLength } from 'class-validator';
+
+@InputType('CreateInquiryInput')
+export class CreateInquiryDto {
+    @Field()
+    @IsString()
+    @Length(2, 100)
+    name!: string;
+
+    @Field()
+    @IsString()
+    @IsEmail()
+    @MaxLength(254)
+    email!: string;
+
+    @Field({ nullable: true })
+    @IsOptional()
+    @IsString()
+    @MaxLength(150)
+    company?: string;
+
+    @Field()
+    @IsString()
+    @Length(10, 2000)
+    message!: string;
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -11,6 +12,15 @@ async function bootstrap() {
     }
 
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
     await app.listen(port, '0.0.0.0');
 }
 
