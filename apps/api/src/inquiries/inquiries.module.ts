9cfcea1d24944a8f34e58bdb88f87154e1359b4e
Michael E. Ershov <i@emershov.ru>
2026-08-29 01:30:49 +0800

    registered repository in module

--- apps/api/src/inquiries/inquiries.module.ts
+++ apps/api/src/inquiries/inquiries.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 import { InquiriesResolver } from './inquiries.resolver';
 import { InquiriesService } from './inquiries.service';
+import { InquiriesRepository } from './repositories/inquiries.repository';
 
 @Module({
-    providers: [InquiriesResolver, InquiriesService],
+    imports: [PrismaModule],
+    providers: [InquiriesResolver, InquiriesService, InquiriesRepository],
 })
 export class InquiriesModule {}
