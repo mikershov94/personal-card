85efb5111287a3436e22deb97048c68424483106
Michael E. Ershov <i@emershov.ru>
2026-08-28 22:16:05 +0800

    fixed tests

--- apps/api/src/prisma/prisma.service.spec.ts
+++ apps/api/src/prisma/prisma.service.spec.ts
@@ -6,11 +6,18 @@ describe('PrismaService', () => {
   let service: PrismaService;
 
   beforeEach(async () => {
+    process.env.DATABASE_URL =
+      'postgresql://prisma:prisma@localhost:5432/prisma';
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [PrismaService],
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(() => {
+    delete process.env.DATABASE_URL;
   });
 
   it('should be defined', () => {
