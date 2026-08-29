a13f6d90f9d06f4d7fd14fab4edc0ce1423ef653
Michael E. Ershov <i@emershov.ru>
2026-08-30 00:29:56 +0800

    resolver added to module

--- apps/api/src/portfolio/portfolio.module.ts
+++ apps/api/src/portfolio/portfolio.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { PortfolioResolver } from './portfolio.resolver';
 import { PortfolioService } from './portfolio.service';
 import { PortfolioRepository } from './repositories/portfolio.repository';
 
 @Module({
-    providers: [PortfolioRepository, PortfolioService],
+    providers: [PortfolioRepository, PortfolioService, PortfolioResolver],
 })
 export class PortfolioModule {}
