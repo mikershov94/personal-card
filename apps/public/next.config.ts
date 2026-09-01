f9eba213b6a361fef971ca7a0580001993db7cf2
Michael E. Ershov <i@emershov.ru>
2026-09-02 00:11:07 +0800

    fixed perfomance

--- apps/public/next.config.ts
+++ apps/public/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     cacheComponents: true,
+    htmlLimitedBots:
+        /[\w-]+-Google|Google-[\w-]+|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|Yeti|googleweblight/i,
 };
 
 export default nextConfig;
