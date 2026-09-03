1672ad35b292ae1b95ffa91bc7801bc781412a80
Michael E. Ershov <i@emershov.ru>
2026-09-03 08:26:07 +0800

    fix public image cache permissions on Timeweb

--- apps/public/next.config.ts
+++ apps/public/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     cacheComponents: true,
+    images: {
+        unoptimized: true,
+    },
     htmlLimitedBots:
         /[\w-]+-Google|Google-[\w-]+|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|Yeti|googleweblight/i,
 };
